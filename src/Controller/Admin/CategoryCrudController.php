7050b5f8cdfb7af4dc680573797fdf1c07d15aeb
GBlok02 <61229884+GBlok02@users.noreply.github.com>
2022-04-22 13:37:27 +0200

    Initial commit

---
+++ src/Controller/Admin/CategoryCrudController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class CategoryCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Category::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            // IdField::new('id'),
+            TextField::new('name'),
+            TextField::new('location'),
+            TextField::new('category'),
+            TextField::new('image'),
+            // TextEditorField::new('description'),
+        ];
+    }
+
+}
